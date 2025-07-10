806a4c74f381ff5cb4e9476d4ef812d82e58cc54
Mike Sweeney <blindx@gmail.com>
2025-07-09 23:36:23 -0400

    Make collapsed/expanded deck view for large TappedOut decks

--- Cogs/TappedOut.py
+++ Cogs/TappedOut.py
@@ -45,7 +45,7 @@ def get_deck_data(stub):
 
     return deck_title, deck_url, fields
 
-def make_deck_embed(deck_title, deck_url, fields):
+def make_deck_embed(deck_title, deck_url, fields, full_view=False):
     embed = discord.Embed(
         color=discord.Color.blue(),
         title=deck_title,
@@ -54,22 +54,43 @@ def make_deck_embed(deck_title, deck_url, fields):
     )
     embed.set_thumbnail(url='https://s.tappedout.net/s1/img/2017-4.85563b67c976.png')
     embed.set_footer(text='TappedOut.net')
-    field_count = 1
     if 'Commander' in fields:
         embed.description = '### Commanders:\n'
         embed.description += '\n'.join(fields['Commander'])
 
     sorted_fields = dict(sorted(fields.items(), key=lambda item: len(item[1]), reverse=True))
 
+    unique_cards = sum(len(c) for c in sorted_fields.values())
+    compact = unique_cards > 50
+    field_count = 1
     for label, cards in sorted_fields.items():
         if label == 'Commander': continue
-        embed.add_field(name=label, value='\n'.join(cards))
+
+        if compact and not full_view:
+            smol = cards[0:5]
+            if len(cards) > 5:
+                smol.append('...')
+            embed.add_field(name=label, value='\n'.join(smol))
+        else:
+            embed.add_field(name=label, value='\n'.join(cards))
         if field_count == 2:
             embed.add_field(name='\u200b', value='\u200b')
             field_count = 0
         field_count += 1
 
-    return embed
+    return embed, compact
+
+class ExpandDeckView(discord.ui.View):
+    def __init__(self, deck_title, deck_url, fields, timeout=None):
+        super().__init__(timeout=timeout)
+        self.deck_title = deck_title
+        self.deck_url = deck_url
+        self.fields = fields
+
+    @discord.ui.button(label='View Full Deck', style=discord.ButtonStyle.primary)
+    async def view_full_deck(self, interaction: discord.Interaction, button: discord.ui.Button):
+        embed, compact = make_deck_embed(self.deck_title, self.deck_url, self.fields, full_view=True)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
 class TappedOut(commands.Cog):
     def __init__(self, bot):
@@ -81,7 +102,10 @@ class TappedOut(commands.Cog):
         if deck_stubs:
             for deck_stub in deck_stubs:
                 deck_title, deck_url, fields = get_deck_data(deck_stub)
-                embed = make_deck_embed(deck_title, deck_url, fields)
-                # data = get_deck_data(deck_stub)
-                # embed = make_embed(data['cards'], deck_stub, data['title'])
-                await message.reply(embed=embed, mention_author=False)
+                embed, compact = make_deck_embed(deck_title, deck_url, fields)
+                if compact:
+                    view = ExpandDeckView(deck_title, deck_url, fields)
+                else:
+                    view = None
+
+                await message.reply(embed=embed, view=view, mention_author=False)
